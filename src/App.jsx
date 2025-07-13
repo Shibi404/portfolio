f893fdaa1aa183ebe650a5b1c199c04acda1022d
Shibi404 <shibisbs123@gmail.com>
2025-07-13 21:37:50 +0530

    Portfolio: Add Home page as landing section for portfolio
    - Added the Home page to serve as the main landing section of the portfolio.
    - Includes structured layout for headline, subheadline, description, and masked profile image.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { react } from 'react'
-import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <Home />
     </>
   )
 }

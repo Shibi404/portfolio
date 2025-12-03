7038e946682d8ffb7fe7db7d2e3fa25d1186456e
Shibi404 <shibisbs123@gmail.com>
2025-12-03 19:28:42 +0530

    Portfolio: Make Active Page Navbar Link Bold

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link, useLocation } from 'react-router-dom';
 import '../styles/navbar.css';
 import logo from '../images/Logo.png';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
 
   const closeMenuAndNavigate = (path) => {
     setIsOpen(false);
@@ -45,16 +46,19 @@ function Navbar() {
         </label>
 
         <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
-          <li className='nav-links-item'>
+          <li className={`nav-links-item ${location.pathname === '/' ? 'active-link' : ''}`}>
             <Link to="/" onClick={() => closeMenuAndNavigate('/')}>Home</Link>
           </li>
-          <li className='nav-links-item'>
+
+          <li className={`nav-links-item ${location.pathname === '/about' ? 'active-link' : ''}`}>
             <Link to="/about" onClick={() => closeMenuAndNavigate('/about')}>About</Link>
           </li>
-          <li className='nav-links-item'>
+
+          <li className={`nav-links-item ${location.pathname === '/projects' ? 'active-link' : ''}`}>
             <Link to="/projects" onClick={() => closeMenuAndNavigate('/projects')}>Projects</Link>
           </li>
-          <li className='nav-links-item'>
+
+          <li className={`nav-links-item ${location.pathname === '/skills' ? 'active-link' : ''}`}>
             <Link to="/skills" onClick={() => closeMenuAndNavigate('/skills')}>Skills</Link>
           </li>
         </ul>

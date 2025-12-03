0e6fff7151f676bbd7be865d2b083971788d261d
Shibi404 <shibisbs123@gmail.com>
2025-12-03 20:10:09 +0530

    Portfolio: Add about me content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,14 +6,42 @@ const About = () => {
     <>
     <section className='about-header'>
         <div className='container about-header-container'>
-            <h2>About</h2>
+            <h2>About Me</h2>
         </div>
+    </section>
+
+    <section>
+        <div className='container'>
+            <div className='about-me-para-container'>
+                <p>
+                    I’m a <strong>full-stack developer</strong> who enjoys building 
+                    <strong> practical</strong>, <strong> user-friendly</strong>, and 
+                    <strong> clean</strong> web applications. I love exploring both 
+                    <strong> frontend and backend technologies</strong>, and I’m always 
+                    <strong> learning new tools, frameworks, and concepts</strong> to improve my skills.
+                </p>
+                <br />
+                <p>
+                    I’m someone who is <strong>determined to complete any task</strong> I take on—
+                    whether it’s fixing a difficult bug, experimenting with new features, 
+                    or diving into something completely new. I believe in 
+                    <strong> consistent growth</strong>, and I’m still in my 
+                    <strong> learning stage</strong>, sharpening my abilities through real projects 
+                    and hands-on practice.
+                </p>
+                <br />
+                <p>
+                    My goal is to <strong>keep improving</strong>, take on 
+                    <strong> challenging projects</strong>, and become 
+                    <strong> better every day</strong> as a developer.
+                </p>
+            </div>
+            </div>
     </section>
 
     <section className='education'>
         <div className='container'>
             <h2 className='hero-heading'>Education</h2>
-
             <div className='education-card-container'>
                 <div className='timeline'>
                     <div className='education-card'>

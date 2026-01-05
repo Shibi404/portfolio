45fde113c8d3e3b7cfcb7ae058550bac29364d87
Shibi404 <shibisbs123@gmail.com>
2026-01-05 21:21:22 +0530

    Portfolio: Add scroll-to-top behavior on route change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from "./components/ScrollToTop";
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -12,6 +13,7 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />

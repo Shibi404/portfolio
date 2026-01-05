72ba0ec94c1d5ab96b4deef48ad0dae5e3c0adf5
Shibi404 <shibisbs123@gmail.com>
2026-01-05 21:11:05 +0530

    Portfolio: Add Projects in Project Page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../styles/projects.css';
+import Card from '../components/Card';
+import projects from '../data/projects.json';
 
 const Projects = () => {
   return (
@@ -8,6 +10,24 @@ const Projects = () => {
         <div className='container projects-header-container'>
             <h2>Projects</h2>
         </div>
+    </section>
+
+    <section className="project-preview">
+      <div className='container'>
+        <div className='cards-container-1'>
+          {projects.map((project, index) => (
+            <Card
+              key={index}
+              image={project.image}
+              title={project.title}
+              description={project.description}
+              techStack={project.techStack}
+              siteLink={project.siteLink}
+              sourceLink={project.sourceLink}
+            />
+          ))}
+        </div>
+      </div>
     </section>
     </>
   )
